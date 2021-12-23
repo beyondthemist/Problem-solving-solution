53df469d9e4e4e8658e4cfd5166c90a8d88001dc
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-23 11:54:05 +0900

    Create solution.py

---
+++ Programmers/lv.1/72410/solution.py
@@ -0,0 +1,36 @@
+import re 
+
+def solution(new_id):
+    #1
+    new_id = new_id.lower()
+    
+    #2
+    new_id = re.sub('[^a-z0-9-_.]+', '', new_id)
+    
+    #3
+    new_id = re.sub('[.]+', '.', new_id)
+    
+    #4
+    if len(new_id) > 0 and new_id[0] == '.':
+        new_id = new_id[1:]
+    if len(new_id) > 0 and new_id[-1] == '.':
+        new_id = new_id[:-1]
+    
+    #5
+    if new_id == '':
+        new_id = 'a'
+    
+    #6
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+    if new_id[0] == '.':
+        new_id = new_id[1:]
+    if new_id[-1] == '.':
+        new_id = new_id[:-1]
+    
+    #7
+    if len(new_id) <= 2:
+        new_id = new_id + (new_id[-1] * (3 - len(new_id)))
+    
+    answer = new_id
+    return answer
