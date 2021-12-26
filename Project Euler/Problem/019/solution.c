55629710787df6a214da09b1825d38b9ccc96507
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-27 08:38:07 +0900

    Create solution.c

---
+++ Project Euler/Problem/019/solution.c
@@ -0,0 +1,44 @@
+// https://euler.synap.co.kr/problem=19
+// https://projecteuler.net/problem=19
+
+#include <stdio.h>
+ 
+int main()
+{
+    int day = 1; 
+    int year, month;
+    int count = 0;
+    int isLeap;
+ 
+    int arr[2][12] = { 
+        { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
+        { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+    };
+ 
+    for (month = 1; month <= 12; ++month) 
+    {
+        day += arr[0][month - 1];
+    }
+    day = 1 + (7 - (day % 7));
+ 
+    for (year = 1901; year <= 2000; ++year) 
+    {
+        isLeap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+ 
+        for (month = 1; month <= 12; ++month)
+        {
+            count += (day == 1);
+ 
+            while (day <= arr[isLeap][month - 1])
+            {
+                day += 7;
+            }
+ 
+            day -= arr[isLeap][month - 1];
+        }
+    }
+ 
+    printf("%d\n", count);
+ 
+    return 0;
+}
