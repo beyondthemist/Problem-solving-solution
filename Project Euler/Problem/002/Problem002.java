9f7ecb149171cbd93e64edde9131e6cb901c1fa6
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-27 06:07:29 +0900

    Create Problem002.java

---
+++ Project Euler/Problem/002/Problem002.java
@@ -0,0 +1,25 @@
+// https://euler.synap.co.kr/problem=2
+// https://projecteuler.net/problem=2
+public class Problem002 {
+ 
+    public static void main(String[] args) {
+        int n1 = 1, n2 = 2;
+        int n3 = n1 + n2;
+        int sum = 2;
+        
+        while(n3 <= 4000000) {
+            n3 = n1 + n2;
+            
+            if(n3%2 == 0) {
+                sum += n3;
+            }
+            
+            n1 = n2;
+            n2 = n3;
+        }
+        
+        System.out.println("Sum:" + sum);
+        
+    }
+ 
+}
