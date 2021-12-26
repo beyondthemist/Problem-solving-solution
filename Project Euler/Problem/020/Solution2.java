680dab1cd4478dfaca2f1d3ecfc25b27577e37a8
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-27 08:39:56 +0900

    Create Solution2.java

---
+++ Project Euler/Problem/020/Solution2.java
@@ -0,0 +1,27 @@
+// https//euler.synap.co.kr/problem=20
+// https://projecteuler.net/problem=20
+
+import java.math.BigInteger;
+ 
+public class Solution2 {
+    
+    public static void main(String[] args) {
+        System.out.println(run());
+    }
+    
+    public static String run() {
+        BigInteger bi = BigInteger.ONE;
+        int result = 0;
+        
+        for(int i = 1; i <= 100; i++) {
+            bi = bi.multiply(new BigInteger(Integer.toString(i)));
+        }
+        
+        while(!bi.equals(BigInteger.ZERO)) {
+            result += bi.mod(BigInteger.TEN).intValue();
+            bi = bi.divide(BigInteger.TEN);
+        }
+            
+        return Integer.toString(result);
+    }
+}
