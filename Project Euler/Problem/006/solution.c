009d1cf19deb9e4ed1542ca8a72f76e2f64801fd
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-27 06:17:31 +0900

    Create solution.c

---
+++ Project Euler/Problem/006/solution.c
@@ -0,0 +1,23 @@
+// https://euler.synap.co.kr/problem=6
+// https://projecteuler.net/problem=6
+
+#include <stdio.h>
+ 
+#define MAX 100
+ 
+int main()
+{
+    printf("Square of sum: %d\n", ((((MAX)*(MAX + 1)) / 2)*(((MAX)*(MAX + 1)) / 2)));
+    printf("Sum of squares: %d\n", ((MAX*(MAX + 1)*(2 * MAX + 1)) / 6));
+    printf("Answer: %d\n", ((((MAX)*(MAX + 1)) / 2)*(((MAX)*(MAX + 1)) / 2)) - ((MAX*(MAX + 1)*(2 * MAX + 1)) / 6));
+  
+    /*
+    int i;
+    int answer = 0;
+ 
+    for (i = 1; i <= MAX; ++i) { answer += ((i * i * i) - (i * i)); }
+ 
+    printf("Answer: %d\n", answer);
+    */ 
+    return 0;
+}
