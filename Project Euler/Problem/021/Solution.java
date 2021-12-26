32e8b065f2b3a07952315c039c60234f5df3e6bc
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-27 08:51:55 +0900

    Create Solution.java

---
+++ Project Euler/Problem/021/Solution.java
@@ -0,0 +1,59 @@
+// https://euler.synap.co.kr/problem=21
+// https://projecteuler.net/problem=21
+
+public class Problem021 {
+    public static void main(String[] args) {
+        System.out.println(run());
+    }
+
+    public static String run() {
+        int sum = 0;
+        int b = 0;
+
+        for(int a = 2; a <= 10000; ++a) {
+            if(!isPrime(a)) {
+                b = d(a);
+                
+                if((b != a) && (d(b) == a)) {
+                    sum += a;
+                }
+            }
+        }
+
+        return Integer.toString(sum);
+    }
+
+    public static int d(int num) {
+        int sum = 1;    
+        int root = (int)(Math.sqrt(num));
+
+        for(int i = 2; i <= root; ++i) { 
+            if(num%i == 0) { 
+                sum += (i + num/i);
+            }
+        }
+
+        return sum;
+    }
+
+    public static boolean isPrime(int num) {
+        if(num <= 1) {
+            return false;
+        } else if(num == 2) { 
+            return true;
+        } else {
+            if(num % 2 == 0) {
+                return false;
+            } else {
+                for (int i = 3; i*i <= num; i += 2) {
+                    if (num % i == 0) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+
+}
