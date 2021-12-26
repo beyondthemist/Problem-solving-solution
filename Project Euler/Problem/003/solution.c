0dc6a22071bbf3bb1a340051e3be3a0f8a687e9a
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-27 06:09:01 +0900

    Create solution.c

---
+++ Project Euler/Problem/003/solution.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ 
+int run();
+ 
+int main()
+{
+    return run();
+}
+ 
+int run()
+{
+    long long number = 600851475143;
+    long long pf = 3;
+    long long max = -1;
+ 
+    while (number > 1)
+    {
+        if (number % pf == 0)
+        {
+            number /= pf;
+
+            if (max < pf) { max = pf; }
+        }
+        else
+        {
+            ++pf;
+        }
+ 
+    }
+ 
+    printf("%lld\n", max);
+ 
+    return 0;
+}
