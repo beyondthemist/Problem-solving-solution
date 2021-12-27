d90c18d29b3f4bcec21e51fa25d513eed369de71
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-28 03:21:30 +0900

    Create solution.c

---
+++ BAEKJOON/04948/solution.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    int input;
+    int count = 0;
+    int i, j;
+    int* result = (int*)malloc(sizeof(int));
+    int idx = 0;
+    int isComp[246913] = { 0 };
+ 
+    for (i = 2; i < sizeof(isComp) / sizeof(int); ++i)
+    {
+        if (isComp[i] == 0)
+        {
+            for (j = 2*i; j < sizeof(isComp) / sizeof(int); j += i)
+            {
+                isComp[j] = 1;
+            }
+        }
+    }
+ 
+    while(1)
+    {
+        scanf("%d", &input);
+        
+        if (!input) { break;}
+ 
+        for (i = (input + 1); i <= (2 * input); ++i)
+        {
+            if (!isComp[i])    { ++count; }
+        }
+ 
+        result = realloc(result, sizeof(int) * (idx + 1));
+        result[idx++] = count;
+        
+        count = 0;
+    } 
+    
+    for (i = 0; i < idx; ++i)
+    {
+        printf("%d\n", result[i]);
+    }
+    
+    return 0;
+}
