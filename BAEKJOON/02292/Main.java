c4f40947d0908c517e3a76813ca0fd524f3e2f69
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-28 02:53:34 +0900

    Create Main.java

---
+++ BAEKJOON/02292/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        System.out.print(new Main().run());
+    }
+    
+    public String run() {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        int count = 1;
+        
+        if(n == 1L) {
+            scanner.close();
+            
+            return "1";
+        }
+        
+        for(long na = 0L, nb = 1L, a = 0L, b = 1L; ; na += a, nb += b) {
+            count++;
+            
+            if((6*na + 2 <= n) && (n <= 6*nb + 1)) {
+                break;
+            }
+            a++;
+            b++;
+            
+        }
+        
+        scanner.close();
+        
+        return Integer.toString(count);
+    }
+ 
+}
