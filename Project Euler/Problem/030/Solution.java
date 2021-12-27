393a9a4c3aa64c00009373a04dab5155cba6ccc4
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-27 09:44:51 +0900

    Create Solution.java

---
+++ Project Euler/Problem/030/Solution.java
@@ -0,0 +1,37 @@
+// https://euler.synap.co.kr/problem=30
+// https://projecteuler.net/problem=30
+
+public class Solution {
+
+	public static void main(String[] args) {
+		System.out.println(run());
+	}
+	
+	public static String run() {
+		long sum = 0;
+		
+		for(int i = 2; i <= Integer.MAX_VALUE; ++i) {
+			if(i == res(i)) { 
+				sum += i; 
+				System.out.println(i);
+			}
+		}
+		
+		return Long.toString(sum);
+	}
+	
+	public static int res(int n) {
+		int sum = 0;
+		
+		while(n != 0) {
+			sum += exp(n % 10);
+			n /= 10;
+		}
+		
+		return sum;
+	}
+	
+	public static int exp(int n) {
+		return (n*n*n*n*n);
+	}
+}
