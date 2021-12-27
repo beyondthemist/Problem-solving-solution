6dad602b7110e625901f346fc5fa325320cd0402
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-28 03:23:15 +0900

    Update and rename BAEKJOON/09020/Solution1/Main.java to BAEKJOON/09020/Main.java

--- BAEKJOON/09020/Main.java
+++ BAEKJOON/09020/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
  
 public class Main {
@@ -9,42 +8,37 @@ public class Main {
     
     public static void run() {
         int t;
-        int[] n;
+        boolean[] isComp = new boolean[10001];
         Scanner scanner = new Scanner(System.in);
         int a = -1, b = -1;
+        int[] n;
         
         t = scanner.nextInt();
         n = new int[t];
         
-        for(int i = 0; i < t; ++i) {
-            n[i] = scanner.nextInt();
+        for(int i = 2; i < isComp.length; ++i) {
+            if(!isComp[i]) {
+                for(int j = i << 1; j < isComp.length; j += i) {
+                    isComp[j] = true;
+                }
+            }
         }
         
         for(int i = 0; i < t; ++i) {
+            n[i] = scanner.nextInt();
+        }    
+        
+        for(int i = 0; i < t; ++i) {
             for(int j = 2; j <= n[i]/2; ++j) {
-                if(isPrime(j) && isPrime(n[i] - j)) {
-                    a = j; b = n[i] - j;
+                if(!(isComp[j] || isComp[n[i] - j])) {
+                    a = j; 
+                    b = n[i] - j;
                 }
             }
-            
+        
             System.out.printf("%d %d\n", a, b);
         }
         
         scanner.close();
     }
-    
-    public static boolean isPrime(int n) {
-        if (n != 2) {
-            if ((n % 2 == 1) && (n != 1)) {
-                for (int i = 3; i*i <= n; i += 2) {
-                    if (n%i == 0) { return false; }
-                }
-            } else { 
-                return false; 
-            }
-        }
- 
-        return true;
-    }
- 
 }
