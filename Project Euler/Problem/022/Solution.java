dc4a9de91a5870b4318581d3e375eb212968baef
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-27 09:06:58 +0900

    Update Solution.java

--- Project Euler/Problem/022/Solution.java
+++ Project Euler/Problem/022/Solution.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
  
-public class Problem022 {
+public class Solution {
     public static void main(String[] args) {
         System.out.print(run());
     }
