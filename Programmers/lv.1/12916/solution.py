9d0ec59714c26ac0a0c03918cef5c333e1892d7a
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-30 02:26:08 +0900

    Update solution.py

--- Programmers/lv.1/12916/solution.py
+++ Programmers/lv.1/12916/solution.py
@@ -1,8 +1,3 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12916
 
 solution = lambda s: s.count('p') + s.count('P') == s.count('y') + s.count('Y')
-
-# or
-# solution = lambda s: s.lower().count('p') == s.lower().count('y')
-# or
-# solution = lambda s: s.upper().count('P') == s.upper().count('Y')
