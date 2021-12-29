f2e307d575c992abe59448b56e600c60bb2559ea
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-30 02:26:17 +0900

    Update another_solution.py

--- Programmers/lv.1/12916/another_solution.py
+++ Programmers/lv.1/12916/another_solution.py
@@ -1,3 +1,4 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12916
 
 solution = lambda s: s.lower().count('p') == s.lower().count('y')
+# solution = lambda s: s.upper().count('P') == s.upper().count('Y')
