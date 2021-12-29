3ad3c946c64fd1066ac446b1186b2de1496b5a54
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2021-12-30 06:02:13 +0900

    Update solution.py

--- Programmers/lv.1/12926/solution.py
+++ Programmers/lv.1/12926/solution.py
@@ -4,12 +4,12 @@ def solution(s, n):
     A_Z = [chr(c) for c in range(ord('A'), ord('Z') + 1)]
     a_z = [chr(c) for c in range(ord('a'), ord('z') + 1)]
     s = list(s)
-    
+
     for i in range(len(s)):
         if s[i] in A_Z:
             s[i] = A_Z[(A_Z.index(s[i]) + n)%len(A_Z)]
         elif s[i] in a_z:
             s[i] = a_z[(a_z.index(s[i]) + n)%len(a_z)]
+
     answer = ''.join(s)
-    
     return answer
