6433ebbd64abd156100154db51cd106f33af4dab
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-01-01 03:46:56 +0900

    Create solution.py

---
+++ Programmers/lv.1/solution.py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12915
+
+def solution(strings, n):
+    d = {}
+    for s in strings:
+        if s[n] not in d.keys():
+            d[s[n]] = [s, ]
+        else:
+            d[s[n]].append(s)
+            
+    for k in d.keys(): #sort words
+        d[k].sort()
+    
+    answer = [ word for words in sorted(d.items(), key = lambda x: x[0]) for word in words[1]]
+    
+    return answer
