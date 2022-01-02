a62da69f2966001c95a1c827a883a61dbbc71ec7
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-01-02 13:01:53 +0900

    Update solution.py

--- Programmers/lv.2/42586/solution.py
+++ Programmers/lv.2/42586/solution.py
@@ -3,13 +3,16 @@
 def solution(progresses, speeds):
     answer = []
     while len(progresses) >= 1:
-        for i in range(len(progresses)):
+        #각 작업의 작업속도 별로 작업 진행
+        for i in range(len(progresses)): 
             progresses[i] += speeds[i]
-
-        if progresses[0] >= 100:
+            
+        #앞의 작업이 완료되면
+        if progresses[0] >= 100: 
             count = 0
-
-            while len(progresses) >= 1 and progresses[0] >= 100:
+            
+            #뒤의 모든 작업과 그의 작업속도를 작업 큐에서 제거함
+            while len(progresses) >= 1 and progresses[0] >= 100: 
                 count += 1
                 progresses.pop(0)
                 speeds.pop(0)
