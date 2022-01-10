00c1de9b4a845b97ece6675877b9910ff5a5b31b
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-01-10 15:33:31 +0900

    Update solution.py

--- Programmers/lv.2/42746/solution.py
+++ Programmers/lv.2/42746/solution.py
@@ -1,7 +1,8 @@
 #https://programmers.co.kr/learn/courses/30/lessons/42746
 
 def solution(numbers):
-    if numbers == [0]*len(numbers):
+    if numbers.count(0) == len(numbers):
         return '0'
-    else:
-        return ''.join(sorted([str(x) for x in numbers], key=lambda x: x*3, reverse=True))
+    
+    numbers = [str(x) for x in numbers]
+    return ''.join(sorted(numbers, key=lambda x: (x*4)[0:4], reverse=True))
