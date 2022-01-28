166b5eb6704c197860a9187d5996a8889c6570b4
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-01-29 00:45:44 +0900

    Create solution.py

---
+++ Programmers/lv.2/81302/solution.py
@@ -0,0 +1,42 @@
+# https://programmers.co.kr/learn/courses/30/lessons/81302
+
+from itertools import combinations
+
+def md(c1, c2):
+    return abs(c2[0] - c1[0]) + abs(c2[1] - c1[1])
+
+def solution(places):
+    answer = []
+    for place in places:
+        placed = []
+        for i in range(len(place)):
+            for j in range(len(place[i])):
+                if place[i][j] == 'P':
+                    placed.append((i, j))
+
+        if placed == []:
+            answer.append(1)
+            continue
+    
+        legal = True
+        for c in combinations(placed, 2):
+            c1 = c[0]
+            c2 = c[1]
+            
+            d = md(c1, c2)
+            if d < 2:
+                legal = False
+            elif d == 2:
+                if c1[0] == c2[0]:
+                    legal = place[c1[0]][max(c1[1], c2[1]) - 1] == 'X'
+                elif c1[1] == c2[1]:
+                    legal = place[max(c1[0], c2[0]) - 1][c1[1]] == 'X'
+                else:
+                    legal = place[c1[0]][c2[1]] == 'X' and place[c2[0]][c1[1]] == 'X'
+                    
+            if not legal:
+                break
+                        
+        answer.append(1 if legal else 0)
+            
+    return answer
