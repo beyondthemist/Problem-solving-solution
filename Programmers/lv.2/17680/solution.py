26b65d023e42dcca646f169ba0e7b7bc7b0adcf2
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-02-14 17:01:27 +0900

    Create solution.py

---
+++ Programmers/lv.2/17680/solution.py
@@ -0,0 +1,23 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17680
+
+def solution(cacheSize, cities):
+    hit = 1; miss = 5
+    if cacheSize == 0:
+        return len(cities) * miss
+    
+    cities = [city.upper() for city in cities]
+    cache = []
+    oldest = 0
+    time = 0
+    for city in cities:
+        if city in cache:
+            time += hit
+            cache.remove(city)
+            cache.append(city)
+        else:
+            if len(cache) >= cacheSize:
+                cache.pop(0)
+            cache.append(city)
+            time += miss
+            
+    return time
