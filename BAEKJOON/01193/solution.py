00d1183428b36b30609f643589d55fd76863db55
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-07-16 20:18:55 +0900

    Create solution.py

---
+++ BAEKJOON/01193/solution.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/1193
+
+def sum(n):
+    return (n*(n + 1))//2
+
+x = int(input())
+
+n = 1
+while sum(n) < x:
+    n += 1
+
+diff = x - sum(n - 1) - 1
+if n % 2 == 0: # ↙
+    print(f'{1 + diff}/{n - (diff)}')
+else: # ↗
+    print(f'{n - (diff)}/{1 + diff}')
