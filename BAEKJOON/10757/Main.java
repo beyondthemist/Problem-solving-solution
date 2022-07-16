b001aba1ee85f04bbf5f0819ecc7dd2b5898cfe2
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-07-16 22:17:04 +0900

    Create Main.java

---
+++ BAEKJOON/10757/Main.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(run());
+    }
+    
+    public static String run() {
+        String[] input = new Scanner(System.in).nextLine().split(" ");
+        
+        BigInteger a = new BigInteger(input[0]);
+        BigInteger b = new BigInteger(input[1]);
+
+        return a.add(b).toString();
+    }
+}
