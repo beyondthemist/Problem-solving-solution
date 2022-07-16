1e7e76876ad91402ffa0961b01bfef201d67d5f9
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-07-16 19:32:40 +0900

    Create solution.py

---
+++ BAEKJOON/02292/solution.py
@@ -0,0 +1,17 @@
+#https://www.acmicpc.net/problem/2292
+
+def solution(n):
+    if n == 1:
+        return -1
+
+    count = 2 #first and last
+    tmp = n - 2
+    i, a = 0, 1
+    while not (6*i <= tmp and tmp < 6*(i + a)):
+        count += 1
+        i += a
+        a += 1
+    return count
+
+n = int(input())
+print(solution(n))
