49ef71ed1d00aaa4aa2acee5e3e535ebb7f00895
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-07-17 16:45:57 +0900

    Update solution.py

--- BAEKJOON/02869/solution.py
+++ BAEKJOON/02869/solution.py
@@ -3,3 +3,7 @@
 from math import ceil
 a, b, v = [int(x) for x in input().split()]
 print(1 + ceil((v - a)/(a - b)))
+
+'''Without importing'''
+# a, b, v = [int(x) for x in input().split()]
+# print(1 + ((v - a)//(a - b)) + (1 if (v - a)%(a - b) != 0 else 0))
