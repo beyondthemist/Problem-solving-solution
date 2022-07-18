dfe3b5bb3ba8bb02df5955b6f35151c97e1f67c1
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-07-18 15:09:53 +0900

    Create solution.py

---
+++ BAEKJOON/01978/solution.py
@@ -0,0 +1,17 @@
+def is_prime(x):
+    if x == 1:
+        return 0
+    elif x == 2:
+        return 1
+    else:
+        for i in range(2, x):
+            if x%i == 0:
+                return 0
+    return 1
+
+
+n = int(input())
+count = 0
+for s in input().split():
+    count += is_prime(int(s))
+print(count)
