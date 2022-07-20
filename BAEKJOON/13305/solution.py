a46031ecac0a86a5d7f291622b90054dab9454d4
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-07-20 17:00:02 +0900

    Create solution.py

---
+++ BAEKJOON/13305/solution.py
@@ -0,0 +1,12 @@
+n = int(input())
+d = [int(x) for x in input().split()]
+price = [int(x) for x in input().split()]
+m, m_idx = price[0], 0
+cost = 0
+for i in range(1, n - 1):
+    if m >= price[i]:
+        cost += m*(sum(d[m_idx:i]))
+        m = price[i]
+        m_idx = i
+cost += m*(sum(d[m_idx:(n - 1)]))
+print(cost)
