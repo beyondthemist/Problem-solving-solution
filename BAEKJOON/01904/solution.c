202929ddc0eb5cdbb832733b01ba60fa5b1b98c0
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-07-21 17:26:12 +0900

    Create solution.c

---
+++ BAEKJOON/01904/solution.c
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/1904
+// I coudn't solve this problem by using Python because of time limit of 750ms.
+#include <stdio.h>
+
+int main()
+{
+    int x = 1, y = 1;
+    int i = 0, temp;
+    int n;
+
+    scanf("%d", &n);
+    for (; i < n - 1; ++i)
+    {
+        temp = x;
+        x = y % 15746;
+        y = (y + temp) % 15746;
+    }
+
+    printf("%d", y);
+
+    return 0;
+}
