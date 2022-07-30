befeaa2573c7a9ac2fcfb76299390e1fae6ba732
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-07-31 00:11:02 +0900

    Create Main.java

---
+++ BAEKJOON/01001/Main.java
@@ -0,0 +1,18 @@
+//https://www.acmicpc.net/problem/1001
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        int n1, n2;
+		    n1 = scanner.nextInt();
+        n2 = scanner.nextInt();
+        
+		System.out.print((n1 - n2));
+		
+		scanner.close();
+	}
+
+}
