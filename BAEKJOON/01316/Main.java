b2b00dd1f23a2f2cc150f5a5011c6e55452aee47
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-07-31 11:25:58 +0900

    Create Main.java

---
+++ BAEKJOON/01316/Main.java
@@ -0,0 +1,33 @@
+// https://www.acmicpc.net/problem/1316
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) { 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int i = 0;
+		int n = sc.nextInt();
+		int answer = n;
+		
+		for(int test_case = 0; test_case < n; test_case++) {
+			String word = sc.next();
+			boolean[] checker = new boolean[26];
+			
+			for(i = 1; i < word.length(); i++) {
+				if(word.charAt(i - 1) != word.charAt(i)) {
+					if(checker[word.charAt(i) - 97] == true) {
+						answer--;
+						break;
+					}
+					
+					checker[word.charAt(i - 1) - 97] = true;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+		
+	}
+}
