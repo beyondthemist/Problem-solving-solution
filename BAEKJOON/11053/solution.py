04529d288f82cfd3b392039f019a473224deaf53
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-08-04 18:23:53 +0900

    Update solution.py

--- BAEKJOON/11053/solution.py
+++ BAEKJOON/11053/solution.py
@@ -2,9 +2,9 @@ n = int(input())
 a = [int(x) for x in input().split()]
 d = [1 for _ in range(n)]
 
-for i in range(n):
-    for j in range(i):
-        if a[i] > a[j]:
-            d[i] = max(d[i], 1 + d[j])
+for curr in range(n):
+    for prev in range(curr):
+        if a[prev] < a[curr] :
+            d[curr] = max(d[curr], 1 + d[prev])
 
 print(max(d))
