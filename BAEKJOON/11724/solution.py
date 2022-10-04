5dd78ac21b62d413977660c4df5970c7447f3f31
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-10-05 00:09:49 +0900

    Update solution.py

--- BAEKJOON/11724/solution.py
+++ BAEKJOON/11724/solution.py
@@ -1,23 +1,24 @@
 import sys
 input = sys.__stdin__.readline
 
-from typing import List, Dict
+from typing import List, Dict, Set
 
-def dfs(graph: Dict[int, List[int]], root: int, visited: List[int]=[]):
+def dfs(graph: Dict[int, List[int]], root: int, visited: Set[int]= set()):
     stack: List[int] = [root]
+    stack_set = set(stack)
     while stack:
         v = stack.pop()
+        stack_set.remove(v)
 
         if v not in visited:
-            visited.append(v)
-
+            visited.add(v)
+            
             for u in graph[v]:
-                if u not in stack and u not in visited:
+                if u not in stack_set and u not in visited:
                     stack.append(u)
-
+                    stack_set.add(u)
+                
             graph[v] = []
-
-    return visited
 
 
 n, m = [int(x) for x in input().split()]
@@ -35,10 +36,10 @@ for _ in range(m):
     else:
         graph[v].append(u)
 
+
 count = n - len(graph.keys()) # nodes that have no edges
 for v in graph.keys():
     if len(graph[v]) > 0:
         dfs(graph, v)
         count += 1
-        
 print(count)
