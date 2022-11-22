08db84e44b67e2698d903b6208785cfd6b669780
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-11-22 15:01:15 +0900

    Update solution.py

--- Programmers/lv.1/68644/solution.py
+++ Programmers/lv.1/68644/solution.py
@@ -1,11 +1,11 @@
 # https://programmers.co.kr/learn/courses/30/lessons/68644
 
 def solution(numbers):
-    answer = []
+    answer = set()
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
             temp = numbers[i] + numbers[j]
             if temp not in answer:
-                answer.append(temp)
+                answer.add(temp)
         
     return sorted(answer)
