7fd4c6cb70a13b0ba21cc305970371b5d5b8dbfd
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-11-22 11:18:57 +0900

    Update solution.py

--- Programmers/lv.1/70128/solution.py
+++ Programmers/lv.1/70128/solution.py
@@ -1,7 +1,10 @@
 #https://programmers.co.kr/learn/courses/30/lessons/70128
+
 def solution(a, b):
     answer = 0
     for x, y in zip(a, b):
         answer += x*y
         
     return answer
+
+#solution = lambda a, b: sum([x*y for x, y in zip(a, b)])
