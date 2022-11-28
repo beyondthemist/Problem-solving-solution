a4cb1d2ac0e5cb4d273a620f1b41d8199efb4496
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-11-28 15:10:54 +0900

    Create solution.py

---
+++ Programmers/lv.1/118666/solution.py
@@ -0,0 +1,29 @@
+def solution(surveys, choices):
+    scores = {'RT': {'R': 0, 'T': 0},
+    'CF': {'C': 0, 'F': 0},
+    'MJ': {'M': 0, 'J': 0},
+    'AN': {'A': 0, 'N': 0}}
+
+    keys = set(scores.keys())
+    for i in range(len(surveys)):
+        if surveys[i] not in keys:
+            surveys[i] = surveys[i][1] + surveys[i][0]
+            if choices[i] > 4:
+                choices[i] -= 4
+            else:
+                choices[i] = 8 - choices[i]
+        elif choices[i] < 4:
+            choices[i] = 4 - choices[i]
+    for survey, choice in zip(surveys, choices):
+        scores[survey][survey[choice//4]] += (choice%4)
+
+    answer = []
+    for score in scores:
+        a = scores[score][score[0]] # R, C, M, A
+        b = scores[score][score[1]] # T, F, J, N
+        if score != 'MJ':
+            answer.append(score[0] if a >= b else score[1])
+        else:
+            answer.append(score[1] if a <= b else score[0])
+
+    return ''.join(answer)
