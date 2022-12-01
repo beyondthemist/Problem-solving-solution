3889b8e1ca902801440393e7305376e86224c8a8
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-12-02 00:34:59 +0900

    Update solution.py

--- Programmers/lv.2/87946/solution.py
+++ Programmers/lv.2/87946/solution.py
@@ -1,42 +1,30 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/87946
+from typing import List, Set
 
-from typing import Set
-
-def solution(k, dungeons):
+def solution(k: int, dungeons: List[int]):
     n = len(dungeons)
 
-    def discover(curr_hp=k, discovered_stage=0, discovered: Set[int]=set()) -> int:
-        # if all dungeons are discovered
-        if discovered_stage >= n:
-            return n
-
-        # Look out dungeon to discover
-        to_discover = list()
+    def explore(curr_hp=k, curr_stage=0, explored: Set[int]=set()) -> int:
+        # Look out dungeons to explore
+        to_explore = list()
         for i in range(n):
             if curr_hp >= dungeons[i][0] \
-                and i not in discovered:
-                to_discover.append(i)
-        
-        # if there is no dungeon to discover
-        if not to_discover:
-            return discovered_stage
+                and i not in explored:
+                to_explore.append(i)
 
+        # if no dungeon to explore then end the exploratory
+        if not to_explore:
+            return curr_stage
 
-        # if there is dungeon to discover
         res = -1
-        for dest_idx in to_discover:
-            x = discover(
+        for dest_idx in to_explore:
+            x = explore(
                 curr_hp=curr_hp - dungeons[dest_idx][1],
-                discovered_stage=discovered_stage + 1,
-                discovered=discovered.union(set([dest_idx]))
+                curr_stage=curr_stage + 1,
+                explored=explored.union(set([dest_idx]))
             )
-
             if res < x:
                 res = x
 
         return res
 
-    return discover()
-
-# print(solution(80, [[80, 20], [50, 40], [700, 10]])) -> 2
-# print(solution(80, [[80, 20], [50, 40], [70, 10]])) -> 2
+    return explore()
