ca03b83c3bb7210ec303aae97fb1b1e57dd9636f
Kiribhi <71578653+Kiribhi@users.noreply.github.com>
2022-12-02 19:31:24 +0900

    Update solution.py

--- Programmers/lv.2/12913/solution.py
+++ Programmers/lv.2/12913/solution.py
@@ -21,7 +21,6 @@ def solution(land: List[List[int]]) -> int:
         #dp[i][2] = land[i][2] + max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][3])
         #dp[i][3] = land[i][3] + max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2])
 
-    print(dp)
     answer = max(dp[-1])
     return answer
 
