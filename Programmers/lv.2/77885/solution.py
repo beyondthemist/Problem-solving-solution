bad8dd4dc3abf2d95b67332c08ab4de4639f57d9
Vol de nuit <71578653+Kiribhi@users.noreply.github.com>
2022-12-14 11:44:35 +0900

    Update solution.py

--- Programmers/lv.2/77885/solution.py
+++ Programmers/lv.2/77885/solution.py
@@ -2,14 +2,14 @@ def solution(numbers):
     answer = []
     for number in numbers:
         if number&1 == 0:
-            answer.append(number+1)
+            answer.append(number + 1)
         else:
             if number == 1:
                 answer.append(2)
             else:
                 b = '0' + bin(number)[2:]
                 i = b.rindex('01')
-                b = b[:i] + '10' + b[i + 2:]
+                b = b[:i] + '10' + b[(i + 2):]
                 answer.append(int(''.join(b), 2))
             
     return answer
