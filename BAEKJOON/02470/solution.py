5c26677e373e8e5c4289b970a0535eb44ce40891
Vol de nuit <71578653+beyondthemist@users.noreply.github.com>
2022-12-19 22:18:35 +0900

    Update solution.py

--- BAEKJOON/02470/solution.py
+++ BAEKJOON/02470/solution.py
@@ -1,26 +1,26 @@
 import sys
 input = sys.__stdin__.readline
 
-def solve(a: list[int], n):
+def solve(solutions: list[int], n: int) -> str:
     left, right = 0, n - 1
 
-    val = a[left] + a[right]
-    answer = f'{a[left]} {a[right]}'
+    val = solutions[left] + solutions[right]
+    answer = f'{solutions[left]} {solutions[right]}'
     while val != 0:
-        if a[left] + a[right] > 0:
+        if solutions[left] + solutions[right] > 0:
             right -= 1
-        elif a[left] + a[right] < 0:
+        elif solutions[left] + solutions[right] < 0:
             left += 1
         
         if left >= right:
             break
 
-        if abs(val) > abs(a[left] + a[right]):
-            val = a[left] + a[right]
-            answer = f'{a[left]} {a[right]}'
+        if abs(val) > abs(solutions[left] + solutions[right]):
+            val = solutions[left] + solutions[right]
+            answer = f'{solutions[left]} {solutions[right]}'
 
     return answer
 
 n = int(input().rstrip())
-a = sorted([int(x) for x in input().split()])
-print(solve(a, n))
+solutions = sorted([int(x) for x in input().split()])
+print(solve(solutions, n))
