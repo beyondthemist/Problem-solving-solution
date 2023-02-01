97f84a7a3531f419fd9157df08b65ed65afc51a1
Vol de nuit <71578653+beyondthemist@users.noreply.github.com>
2023-02-01 14:23:11 +0900

    Create solution.py

---
+++ BAEKJOON/09657/solution.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.__stdin__.readline
+
+n = int(input())
+dp = [True, False, True, True]
+
+if n < 4:
+    print('SK' if dp[n - 1] else 'CY')
+else:
+    for i in range(4, n):
+        dp.append((not dp[-1]) or (not dp[-3]) or (not dp[-4]))
+    print('SK' if dp[-1] else 'CY')
