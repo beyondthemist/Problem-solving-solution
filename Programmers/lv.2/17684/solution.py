f58cd149670170aff167c003c4b0621b9df1ed86
Vol de nuit <71578653+beyondthemist@users.noreply.github.com>
2023-02-21 23:09:58 +0900

    Create solution.py

---
+++ Programmers/lv.2/17684/solution.py
@@ -0,0 +1,21 @@
+def solution(msg):
+    answer = []
+    d = [chr(c) for c in range(65, 91)]
+    l = len(msg)
+    i = 0
+    while i < l:
+        j = i + 1
+        x = msg[i:j]
+        while j < l and x in d:
+            j += 1
+            x = msg[i:j]
+
+        if x in d:
+            answer.append(d.index(x) + 1)
+            i += j - i
+        else:
+            d.append(x)
+            answer.append(d.index(x[:-1]) + 1)
+            i += j - i - 1
+
+    return answer
