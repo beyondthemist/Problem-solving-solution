a1f97b22c38d6f65ac5cdf050f81775a9117be6d
Vol de nuit <71578653+beyondthemist@users.noreply.github.com>
2023-03-02 18:49:23 +0900

    Update solution.py

--- Programmers/lv.2/140107/solution.py
+++ Programmers/lv.2/140107/solution.py
@@ -1,9 +1,17 @@
 from math import sqrt
 
 def solution(k, d):
+    return sum([((int(sqrt(d**2 - x**2))//k) + 1) for x in range(0, d + 1, k)])
+
+
+'''
+from math import sqrt
+
+def solution(k, d):
     answer = 0
     
     for x in range(0, d + 1, k):
         answer += ((int(sqrt(d**2 - x**2))//k) + 1)
         
     return answer
+'''
