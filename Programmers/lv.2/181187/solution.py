8c07a1c2f39613a0aaa9f30a21d21ee13bdea39d
Vol de nuit <71578653+beyondthemist@users.noreply.github.com>
2023-07-10 10:02:21 +0900

    Update solution.py

--- Programmers/lv.2/181187/solution.py
+++ Programmers/lv.2/181187/solution.py
@@ -1,12 +1,12 @@
 from math import ceil, floor, sqrt
 
 def solution(r1, r2):
-    answer = 0
+    answer = 2*(r2 - r1 + 1)
     
     for x in range(1, r1):
-        answer += 2*(floor(sqrt((r2**2) - (x**2))) - ceil(sqrt((r1**2) - (x**2))) + 1)
+        answer += 4*(floor(sqrt((r2**2) - (x**2))) - ceil(sqrt((r1**2) - (x**2))) + 1)
     
     for x in range(r1, r2 + 1):
-        answer += ((2*(floor(sqrt((r2**2) - (x**2))))) + 1)
+        answer += 2*(2*(floor(sqrt((r2**2) - (x**2)))) + 1)
 
-    return 2*answer + 2*(r2 - r1 + 1)
+    return answer
